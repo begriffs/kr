87502079cedb461f267fd8871422e6bcce525929
Joe Nelson <cred+github@begriffs.com>
2018-03-06 23:54:19 -0600

    Tab programs

---
+++ 1-10-detab.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define TABSTOP 4
+
+int main()
+{
+  int c, col, delta;
+
+  col = 0;
+  while ((c = getchar()) != EOF) {
+    if(c == '\t') {
+      delta = TABSTOP - (col % TABSTOP);
+      col = col + delta;
+      for(; delta > 0; --delta)
+        putchar(' ');
+    } else {
+      if (c == '\n')
+        col = 0;
+      else
+        ++col;
+      putchar(c);
+    }
+  }
+
+  return 0;
+}
---
+++ 1-10-entab.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define TABSTOP 4
+
+int main()
+{
+  int c, col, sp, i;
+
+  sp = col = 0;
+  while ((c = getchar()) != EOF) {
+    if(c == ' ') {
+      ++sp;
+    } else {
+      for(i = 0; i < sp / TABSTOP; ++i)
+        putchar('\t');
+      for(i = 0; i < sp % TABSTOP; ++i)
+        putchar(' ');
+      sp = 0;
+      putchar(c);
+    }
+  }
+
+  return 0;
+}
