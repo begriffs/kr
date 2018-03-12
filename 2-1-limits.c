44c125a0b1e1b81953975c0467785edc15d6c535
Joe Nelson <cred+github@begriffs.com>
2018-03-11 20:12:05 -0500

    Some wimpy ch2 exercises

---
+++ 2-1-limits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	printf("char : %lu\n", sizeof(char));
+	printf("short: %lu\n", sizeof(short));
+	printf("int  : %lu\n", sizeof(int));
+	printf("long : %lu\n", sizeof(long));
+
+	printf("max signed char : %d\n", CHAR_MAX);
+	printf("max signed short: %d\n", SHRT_MAX);
+	printf("max signed int  : %d\n", INT_MAX);
+	printf("max signed long : %ld\n", LONG_MAX);
+
+	printf("max unsigned char : %u\n", UCHAR_MAX);
+	printf("max unsigned short: %u\n", USHRT_MAX);
+	printf("max unsigned int  : %u\n", UINT_MAX);
+	printf("max unsigned long : %lu\n", ULONG_MAX);
+
+	printf("min signed char : %d\n", CHAR_MIN);
+	printf("min signed short: %d\n", SHRT_MIN);
+	printf("min signed int  : %d\n", INT_MIN);
+	printf("min signed long : %ld\n", LONG_MIN);
+
+	return 0;
+}
