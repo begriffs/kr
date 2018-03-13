69af8baee0d6296af259b403e430f4ebfa3749a4
Joe Nelson <cred+github@begriffs.com>
2018-03-12 20:24:55 -0500

    Invert inner bits

---
+++ 2-9-invert.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* Returns x with the n bits that begin at position p
+   all inverted */
+unsigned int
+invert(unsigned int x, int p, int n)
+{
+	/*
+	~(~0 << 3) << p-3
+	0000000000000001110000000000
+	*/
+
+	return x ^ (~(~0 << n) << (p-n));
+}
+
+void printb(unsigned u) {
+	if (u) {
+		printb(u >> 1);
+		if (u & 1)
+			putchar('1');
+		else
+			putchar('0');
+	}
+}
+
+int main() {
+	int i, j;
+	for (i = 0; i < 16; i++) {
+		printb(i);
+		printf("^=");
+		printb(invert(i, 3, 2));
+		putchar('\n');
+	}
+}
