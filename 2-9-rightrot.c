a959d1ee2072a9593dd5ad47d7fc1f95952f60c9
Joe Nelson <cred+github@begriffs.com>
2018-03-12 20:47:14 -0500

    rightrot

---
+++ 2-9-rightrot.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Rotate (not shift) rightward by n bits */
+unsigned int
+rightrot(unsigned int x, int n)
+{
+	int s = sizeof(x)*8;
+	return (x >> n) | (x << (s-n));
+}
+
+void printb(unsigned u) {
+	if (u) {
+		printb(u >> 1);
+		if (u & 1)
+			putchar('1');
+		else
+			putchar('0');
+	}
+}
+
+int main() {
+	int i, j;
+	for (i = 0; i < 16; i++) {
+		printb(+i);
+		printf("\t->\t");
+		printb(rightrot(i, 2));
+		putchar('\n');
+	}
+}
