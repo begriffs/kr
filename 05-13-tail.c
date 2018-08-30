346b4ff62de15b980eb19422551fabfe35391ca6
Joe Nelson <cred+github@begriffs.com>
2018-08-29 20:18:20 -0500

    tail

---
+++ 05-13-tail.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+#define MAXSZ 2048
+
+char buf[MAXSZ], *lines[MAXSZ];
+
+int main(int argc, char **argv)
+{
+	int i = 0, n = 10, c;
+	char *writer;
+
+	if (argc > 1 && *argv[1] == '-')
+		n = MAX(0, atoi(argv[1] + 1));
+
+	lines[0] = writer = buf;
+	while ((c = getchar()) != EOF)
+	{
+		*writer++ = c;
+		if (c == '\n')
+			lines[++i] = writer;
+	}
+	*writer = '\0';
+
+	printf("%s", lines[MAX(0, i-n)]);
+	
+	return 0;
+}
